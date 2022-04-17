feb61e39b3d83b86e1a995a3de40e0e2f919da0b
mosheurpau <mosheur.pau@gamil.com>
2022-04-18 00:18:59 +0600

    added login form

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,10 +1,29 @@
 import React from "react";
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
 
 const Login = () => {
   return (
-    <div>
-      <h2>This is Login</h2>
-    </div>
+    <Container>
+      <Row className="justify-content-md-center">
+        <Col xs="10" lg="6" className="mt-5 mx-auto">
+          <h2 className="text-center my-3">Please Login</h2>
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" required />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" required />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
