a47619e3ec482b45e83f987ae9c9f1e29c90b79a
mosheurpau <mosheur.pau@gamil.com>
2022-04-18 00:41:57 +0600

    added login authentication useing firebase hook

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,24 +1,46 @@
 import React from "react";
 import { Button, Col, Container, Form, Row } from "react-bootstrap";
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
+import auth from "../../../firebase.init";
 
 const Login = () => {
+  const [createUserWithEmailAndPassword, user, loading, error] =
+    useCreateUserWithEmailAndPassword(auth);
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    createUserWithEmailAndPassword(email, password);
+    console.log(email, password);
+  };
   return (
     <Container>
       <Row className="justify-content-md-center">
         <Col xs="10" lg="6" className="mt-5 mx-auto">
           <h2 className="text-center my-3">Please Login</h2>
-          <Form>
+          <Form onSubmit={handleLogin}>
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" required />
+              <Form.Control
+                type="email"
+                name="email"
+                placeholder="Enter email"
+                required
+              />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" required />
+              <Form.Control
+                type="password"
+                name="password"
+                placeholder="Password"
+                required
+              />
             </Form.Group>
-            <Button variant="primary" type="submit">
-              Submit
+            <Button variant="primary" type="submit" className="w-100">
+              Login
             </Button>
           </Form>
         </Col>
