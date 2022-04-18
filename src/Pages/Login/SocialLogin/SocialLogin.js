a70b29ddb6862b397b70aa86d73643307eb44a8d
mosheurpau <mosheur.pau@gamil.com>
2022-04-18 15:55:19 +0600

    added github sign in

--- src/Pages/Login/SocialLogin/SocialLogin.js
+++ src/Pages/Login/SocialLogin/SocialLogin.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { Button, Col, Container, Row } from "react-bootstrap";
-import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import {
+  useSignInWithGithub,
+  useSignInWithGoogle,
+} from "react-firebase-hooks/auth";
 import auth from "../../../firebase.init";
 import google from "../../../images/socialIcon/google.png";
+import github from "../../../images/socialIcon/github.png";
 
 const SocialLogin = () => {
   const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  const [signInWithGithub, user1, loading1, error1] = useSignInWithGithub(auth);
   return (
     <Container>
       <Row className="justify-content-md-center">
@@ -19,10 +24,18 @@ const SocialLogin = () => {
             <Button
               variant="outline-primary"
               onClick={() => signInWithGoogle()}
-              className="w-50 d-block mx-auto my-2"
+              className="w-75 d-block mx-auto my-2"
             >
               <img style={{ width: "30px" }} src={google} alt="" />
               <span className="px-2">Google Sign In</span>
+            </Button>
+            <Button
+              variant="outline-primary"
+              onClick={() => signInWithGithub()}
+              className="w-75 d-block mx-auto my-2"
+            >
+              <img style={{ width: "30px" }} src={github} alt="" />
+              <span className="px-2">Github Sign In</span>
             </Button>
           </div>
         </Col>
